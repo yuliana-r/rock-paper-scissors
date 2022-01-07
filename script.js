7aad0c2c790c3daefa6411cb75d40491e7974ddc
yuliana-r <iuliana.rotaru234@gmail.com>
2022-01-07 19:59:06 +0000

    Initialize playRound()

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let array = [
-    "Rock",
-    "Paper",
-    "Scissors"
+    "rock",
+    "paper",
+    "scissors"
 ]
 
 function computerPlay() {
-    let item = array[Math.floor(Math.random()*array.length)];
-    console.log
+    let item = array[Math.floor(Math.random() * array.length)];
+    return item;
 }
+
+const playerSelection = prompt("Pick Rock, Paper or Scissors!");
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+function playRound(playerSelection, computerSelection) {
+    /* If player picks ROCK */
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+        alert(`Computer chose ${computerSelection}. You lose! Paper beats rock`);
+
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "rock") {
+        alert(`Computer chose ${computerSelection}. It's a draw!`);
+
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors"){} 
+    }
