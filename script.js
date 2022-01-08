fbe3333143a364bbbe3217d1a8a5fb7b8d8e37e1
yuli-r <iuliana.rotaru234@gmail.com>
2022-01-08 15:47:23 +0000

    Finish playRound()

--- script.js
+++ script.js
@@ -14,12 +14,42 @@ const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
 
 function playRound(playerSelection, computerSelection) {
-    /* If player picks ROCK */
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
-        alert(`Computer chose ${computerSelection}. You lose! Paper beats rock`);
 
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "rock") {
-        alert(`Computer chose ${computerSelection}. It's a draw!`);
+    /* If DRAW */
 
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors"){} 
-    }
+    if (playerSelection.toLowerCase() === computerSelection) {
+        alert("It's a draw!");
+    } else {
+        
+        /* If player picks ROCK */
+
+        if (playerSelection.toLowerCase() === "rock") {
+            if (computerSelection === "paper") {
+                alert("You lost! Paper beats rock.");
+            } else {
+                alert("You win! Rock beats scissors. comp choice");
+            }
+
+            /* If player picks PAPER */
+
+        } else if (playerSelection.toLowerCase() === "paper") {
+                if (computerSelection === "rock") {
+                    alert("You win! Paper beats rock.");
+                } else {
+                    alert("You lost! Scissors beat paper.");
+                }
+            }
+
+            /* If player picks SCISSORS */
+
+            else if (playerSelection.toLowerCase() === "scissors") {
+                if (computerSelection === "paper") {
+                    alert("You win! Scissors beat paper.");
+                } else {
+                    alert("You lost! Rock beats scissors.");
+                }
+            } else {
+                alert("I do not understand, please check the spelling and try again!");
+            }
+        }  
+        }
